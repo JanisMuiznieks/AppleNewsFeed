a63353be6805e257be41d864fe84b38a0e6a5559
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-12 15:46:45 +0200

    2021-02-11 Apple news

---
+++ AppleNewsFeed/Model/Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  AppleNewsFeed
+//
+//  Created by janis.muiznieks on 12/02/2021.
+//
+
+import UIKit
+import Gloss
+
+struct Item: JSONDecodable{
+    var description: String
+    var title: String
+    var url: String
+    
+    init?(json: JSON) {
+        self.title = "title" <~~ json ?? ""
+        self.description = "description" <~~ json ?? ""
+        self.url = "url" <~~ json ?? ""
+    }
+}
